8c629181597760bde3effc6f9a94aaa325ac6ef3
Adrián Godoy <adriangodoymartinez@icloud.com>
2019-01-22 12:11:32 +0000

    Kata5P2

---
+++ src/main/Kata5P2.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import static java.util.Collections.list;
+import java.util.List;
+import model.Histogram;
+import model.Mail;
+import view.MailHistogramBuilder;
+import view.MailListReader;
+import view.HistogramDisplay;
+import view.MailListReaderBD;
+
+public class Kata5P2 {
+
+    private Histogram<String> histo;
+    private List<String> list;
+
+    public static void main(String[] args) throws IOException, SQLException{
+        Kata5P2 kata5 = new Kata5P2();
+        kata5.execute();
+    }
+
+    private void execute() throws IOException, SQLException {
+        input();
+        process();
+        output();
+    }
+
+    private void input() throws IOException, SQLException {
+        MailListReader mlr = new MailListReader();
+        list  = new MailListReaderBD().read();
+
+    }
+
+    private void process() {
+        MailHistogramBuilder builder = new MailHistogramBuilder();
+        histo = builder.getHisto(list);
+    }
+
+    private void output() {
+        HistogramDisplay histoD = new HistogramDisplay(histo);
+        histoD.execute();
+    }
+}
